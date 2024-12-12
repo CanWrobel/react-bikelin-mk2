75a745cdbb7f78fac8b8feaf5b3b9cb6a49d8dc8
Can <can.wrob@gmail.com>
2024-12-12 16:00:15 +0100

    login geht

--- react-ts-vite-bikelin/src/components/header/BurgerMenu.tsx
+++ react-ts-vite-bikelin/src/components/header/BurgerMenu.tsx
@@ -1,15 +1,35 @@
 import React from 'react';
-import { keycloak, initKeycloak } from '../../services/auth-service';
+import { initKeycloak } from '../../services/auth-service';
+import keycloak from '../../services/auth-service';
 
-const BurgerMenu: React.FC<{ toggleMenu: () => void }> = ({ toggleMenu }) => {
+interface BurgerMenuProps {
+  toggleMenu: () => void; // Typdefinition für toggleMenu
+}
+
+const BurgerMenu: React.FC<BurgerMenuProps> = ({ toggleMenu }) => {
+  const handleLogin = () => {
+    initKeycloak().then(authenticated => {
+      if (authenticated) {
+        console.log('Authenticated');
+        // Hier kannst du zum Beispiel eine Weiterleitung durchführen oder den Anwendungsstatus aktualisieren
+      } else {
+        console.log('Not authenticated');
+        keycloak.login(); // Leitet den Nutzer zur Keycloak-Login-Seite
+      }
+    }).catch(error => {
+      console.error('Keycloak login error:', error);
+    });
+  };
+
   return (
     <div>
       <button onClick={toggleMenu}>Close</button>
-      <a href="/">Home</a> {/* Jeder Link auf einer neuen Zeile */}
+      <a href="/">Home</a>
       <a href="/about">About Us</a>
       <a href="/contact">Contact Us</a>
+      <button onClick={handleLogin}>Login</button>
     </div>
   );
-}
+};
 
 export default BurgerMenu;
