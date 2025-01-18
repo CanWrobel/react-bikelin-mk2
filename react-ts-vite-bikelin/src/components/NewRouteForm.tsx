d66fc53307127026f585dc3b37105b81cd93dfc5
Can <can.wrob@gmail.com>
2025-01-18 13:31:58 +0100

    places geht jetzte. next step: select und adresse angleichn

--- react-ts-vite-bikelin/src/components/NewRouteForm.tsx
+++ react-ts-vite-bikelin/src/components/NewRouteForm.tsx
@@ -24,135 +24,155 @@ const NewRouteForm: React.FC<NewRouteFormProps> = ({ onClose, onPickLocation }) 
     description: ''
   });
 
-  const startSearchBoxRef = useRef<google.maps.places.SearchBox>(null);
-  const endSearchBoxRef = useRef<google.maps.places.SearchBox>(null);
+  const [startSearchBox, setStartSearchBox] = useState<google.maps.places.SearchBox | null>(null);
+  const [endSearchBox, setEndSearchBox] = useState<google.maps.places.SearchBox | null>(null);
 
-  useEffect(() => {
-    if (startSearchBoxRef.current) {
-      startSearchBoxRef.current.addListener('places_changed', () => handlePlacesChanged(startSearchBoxRef, 'start'));
-    }
-    if (endSearchBoxRef.current) {
-      endSearchBoxRef.current.addListener('places_changed', () => handlePlacesChanged(endSearchBoxRef, 'end'));
-    }
+  const onStartLoad = (ref: google.maps.places.SearchBox) => {
+    console.log('Start SearchBox loaded:', ref);
+    setStartSearchBox(ref);
+  };
 
-    // Cleanup listener when component unmounts or ref changes
-    return () => {
-      if (startSearchBoxRef.current) {
-        google.maps.event.clearInstanceListeners(startSearchBoxRef.current);
+  const onEndLoad = (ref: google.maps.places.SearchBox) => {
+    console.log('End SearchBox loaded:', ref);
+    setEndSearchBox(ref);
+  };
+
+  const handleStartPlacesChanged = () => {
+    console.log('Start places changed');
+    if (startSearchBox) {
+      const places = startSearchBox.getPlaces();
+      console.log('Start places:', places);
+      handlePlaceSelection(places, 'start');
+    }
+  };
+
+  const handleEndPlacesChanged = () => {
+    console.log('End places changed');
+    if (endSearchBox) {
+      const places = endSearchBox.getPlaces();
+      console.log('End places:', places);
+      handlePlaceSelection(places, 'end');
+    }
+  };
+
+  const handlePlaceSelection = (places: google.maps.places.PlaceResult[] | undefined, type: 'start' | 'end') => {
+    if (places && places.length > 0) {
+      const place = places[0];
+      console.log(`Selected ${type} place:`, place);
+
+      const postalCode = place.address_components?.find(
+        component => component.types.includes('postal_code')
+      )?.long_name || '';
+
+      console.log(`Found postal code for ${type}:`, postalCode);
+
+      if (type === 'start') {
+        setRouteData({
+          ...routeData,
+          startAddress: place.formatted_address || '',
+          startPostalCode: postalCode,
+          startPoint: place.geometry?.location ? 
+            `${place.geometry.location.lat()},${place.geometry.location.lng()}` : ''
+        });
+      } else {
+        setRouteData({
+          ...routeData,
+          endAddress: place.formatted_address || '',
+          endPostalCode: postalCode,
+          endPoint: place.geometry?.location ? 
+            `${place.geometry.location.lat()},${place.geometry.location.lng()}` : ''
+        });
       }
-      if (endSearchBoxRef.current) {
-        google.maps.event.clearInstanceListeners(endSearchBoxRef.current);
-      }
-    };
-  }, []);
+    }
+  };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     setRouteData({ ...routeData, [e.target.name]: e.target.value });
   };
 
-  const handlePlacesChanged = (ref, type) => {
-    const places = ref.current?.getPlaces();
-    if (places && places.length > 0) {
-      const place = places[0];
-      const components = place.address_components;
-      const postalCodeObj = components.find(c => c.types.includes('postal_code'));
-      const postalCode = postalCodeObj ? postalCodeObj.long_name : '';
-  
-      if (type === 'start') {
-        setRouteData(prev => ({
-          ...prev,
-          startAddress: place.formatted_address,
-          startPostalCode: postalCode
-        }));
-      } else {
-        setRouteData(prev => ({
-          ...prev,
-          endAddress: place.formatted_address,
-          endPostalCode: postalCode
-        }));
-      }
-    }
-  };
-  
-
   if (!isLoaded) return <div>Loading...</div>;
 
   return (
-<form>
-  <h3>Start Point Details</h3>
-  <StandaloneSearchBox ref={startSearchBoxRef} onLoad={() => startSearchBoxRef.current}>
-    <input
-      type="text"
-      name="startAddress"
-      placeholder="Address"
-      value={routeData.startAddress}
-      onChange={handleChange}
-    />
-  </StandaloneSearchBox>
+    <form>
+      <h3>Start Point Details</h3>
+      <StandaloneSearchBox
+        onLoad={onStartLoad}
+        onPlacesChanged={handleStartPlacesChanged}
+      >
+        <input
+          type="text"
+          name="startAddress"
+          placeholder="Address"
+          value={routeData.startAddress}
+          onChange={handleChange}
+        />
+      </StandaloneSearchBox>
 
-  <input
-    type="text"
-    name="startPostalCode"
-    placeholder="Postal Code"
-    value={routeData.startPostalCode}
-    onChange={handleChange}
-  />
-  <input
-    type="button"
-    value="Select on Map"
-    onClick={() => onPickLocation('start', routeData.startPoint)}
-  />
-  <input
-    type="text"
-    name="startPoint"
-    placeholder="Coordinates"
-    value={routeData.startPoint}
-    readOnly
-  />
+      <input
+        type="text"
+        name="startPostalCode"
+        placeholder="Postal Code"
+        value={routeData.startPostalCode}
+        onChange={handleChange}
+      />
+      <input
+        type="button"
+        value="Select on Map"
+        onClick={() => onPickLocation('start', routeData.startPoint)}
+      />
+      <input
+        type="text"
+        name="startPoint"
+        placeholder="Coordinates"
+        value={routeData.startPoint}
+        readOnly
+      />
 
-  <h3>End Point Details</h3>
-  <StandaloneSearchBox ref={endSearchBoxRef} onLoad={() => endSearchBoxRef.current}>
-    <input
-      type="text"
-      name="endAddress"
-      placeholder="Address"
-      value={routeData.endAddress}
-      onChange={handleChange}
-    />
-  </StandaloneSearchBox>
+      <h3>End Point Details</h3>
+      <StandaloneSearchBox
+        onLoad={onEndLoad}
+        onPlacesChanged={handleEndPlacesChanged}
+      >
+        <input
+          type="text"
+          name="endAddress"
+          placeholder="Address"
+          value={routeData.endAddress}
+          onChange={handleChange}
+        />
+      </StandaloneSearchBox>
 
-  <input
-    type="text"
-    name="endPostalCode"
-    placeholder="Postal Code"
-    value={routeData.endPostalCode}
-    onChange={handleChange}
-  />
-  <input
-    type="button"
-    value="Select on Map"
-    onClick={() => onPickLocation('end', routeData.endPoint)}
-  />
-  <input
-    type="text"
-    name="endPoint"
-    placeholder="Coordinates"
-    value={routeData.endPoint}
-    readOnly
-  />
+      <input
+        type="text"
+        name="endPostalCode"
+        placeholder="Postal Code"
+        value={routeData.endPostalCode}
+        onChange={handleChange}
+      />
+      <input
+        type="button"
+        value="Select on Map"
+        onClick={() => onPickLocation('end', routeData.endPoint)}
+      />
+      <input
+        type="text"
+        name="endPoint"
+        placeholder="Coordinates"
+        value={routeData.endPoint}
+        readOnly
+      />
 
-  <h3>Route Description</h3>
-  <textarea
-    name="description"
-    style={{ width: '100%', boxSizing: 'border-box' }}
-    value={routeData.description}
-    onChange={handleChange}
-    wrap="soft"
-  />
-  <button type="submit">Calculate Route</button>
-  <button type="button" onClick={onClose}>Cancel</button>
-</form>
-
+      <h3>Route Description</h3>
+      <textarea
+        name="description"
+        style={{ width: '100%', boxSizing: 'border-box' }}
+        value={routeData.description}
+        onChange={handleChange}
+        wrap="soft"
+      />
+      <button type="submit">Calculate Route</button>
+      <button type="button" onClick={onClose}>Cancel</button>
+    </form>
   );
 };
 
